3b53b6b92fb1ccbb564bad5447cc32a45e592d13
Thomas Sarlandie <thomas@sarlandie.net>
2018-08-17 17:04:22 +0200

    fix: make storybook great again

--- logbook-viewer/.storybook/webpack.config.js
+++ logbook-viewer/.storybook/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
+
 module.exports = (baseConfig, env, defaultConfig) => {
   defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -7,5 +8,10 @@ module.exports = (baseConfig, env, defaultConfig) => {
   });
   defaultConfig.plugins.push(new TSDocgenPlugin());
   defaultConfig.resolve.extensions.push('.ts', '.tsx');
+
+  // ggencoder has a `if (runningAsMain) require(test)
+  // and the test requires a node library ('fs')
+  defaultConfig.module.noParse = /ggencoder\/test/
+
   return defaultConfig;
 };
