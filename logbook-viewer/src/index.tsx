d46277edc4e6463365ac6a2181804410199c9352
Thomas Sarlandie <thomas@sarlandie.net>
2018-08-21 15:44:23 +0200

    clean: disable serviceWorker and registerObserver
    ServiceWorker does not work in production without a HTTPS endpoint and
    we do not have one. That will be one less error in the console.
    
    react-perf-devtool is not used anymore.

--- logbook-viewer/src/index.tsx
+++ logbook-viewer/src/index.tsx
@@ -2,13 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-import { registerObserver } from 'react-perf-devtool'
-registerObserver()
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+
+// Disable service worker. They do not work without HTTPS on Chrome anyway.
+// registerServiceWorker();
