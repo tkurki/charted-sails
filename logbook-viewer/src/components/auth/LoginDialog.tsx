7e4225a96973e5cd7cd60ed31cb65b80904ab178
Thomas Sarlandie <thomas@sarlandie.net>
2018-10-18 10:32:48 +0200

    feature: improve look of login dialog with facebook

--- logbook-viewer/src/components/auth/LoginDialog.tsx
+++ logbook-viewer/src/components/auth/LoginDialog.tsx
@@ -1,4 +1,4 @@
-import { Button, Callout, Classes, Dialog, FormGroup, InputGroup, Intent, Spinner } from '@blueprintjs/core';
+import { Button, Callout, Classes, Dialog, FormGroup, InputGroup, Intent, Spinner, Tab, Tabs } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import * as React from 'react';
 import './LoginDialog.css';
@@ -15,6 +15,7 @@ interface LoginDialogState {
   username: string
   password: string
   loading: boolean
+  selectedProvider: 'email' | 'facebook'
   errorMessage: string | null
 }
 
@@ -25,6 +26,7 @@ export default class LoginDialog extends React.Component<LoginDialogProps, Login
     this.state = {
       username: '',
       password: '',
+      selectedProvider: "email",
       loading: false,
       errorMessage: null
     }
@@ -43,36 +45,77 @@ export default class LoginDialog extends React.Component<LoginDialogProps, Login
       )
     }
     else {
-    return (
-      <Dialog isOpen={this.props.isOpen}>
-        <div className={Classes.DIALOG_BODY}>
-          {this.state.errorMessage !== null && <Callout intent={Intent.DANGER}>
-            {this.state.errorMessage}
-          </Callout> }
-
-          <FormGroup
-              label="Email"
-          >
-            <InputGroup id="username-input" placeholder="youremail@sailing.dream" leftIcon={ IconNames.USER }
-              value={this.state.username} onChange={ (e:any) => this.setState({ username: e.target.value }) }/>
-          </FormGroup>
-          <FormGroup
-              label="Password"
-          >
-            <InputGroup id="password-input" type="password" leftIcon={ IconNames.LOCK }
-              value={this.state.password} onChange={ (e:any) => this.setState({ password: e.target.value }) }/>
-          </FormGroup>
-
-          <div className="login-dialog-buttons">
-            <Button onClick={ () => this.props.onDismiss() }>Cancel</Button>
-            <Button icon={IconNames.LOG_IN} onClick={ () => this.createAccountWithEmailAndPassword() }>Create account</Button>
-            { this.props.onLoginWithFacebook && <Button icon={IconNames.LOG_IN} onClick={ () => this.loginWithFacebook() }>Login with Facebook</Button> }
-            <Button icon={IconNames.LOG_IN} intent={Intent.PRIMARY} onClick={ () => this.loginWithEmailAndPassword() }>Log in</Button>
+      return (
+        <Dialog isOpen={this.props.isOpen} icon={ IconNames.USER } title="Log In or Sign Up">
+          <div className={Classes.DIALOG_BODY}>
+            { this.renderLoginDialogContent() }
           </div>
-        </div>
-      </Dialog>
+        </Dialog>
       )
     }
+  }
+
+  private renderLoginDialogContent() {
+    if (this.props.onLoginWithFacebook) {
+      return (
+            <Tabs
+              animate={false}
+              id="login-method"
+              selectedTabId={ this.state.selectedProvider }
+              onChange={ (tabId) => this.setState({ selectedProvider: tabId as ("email"|"facebook") })}
+            >
+              <Tab id="email" title="Email" panel={ this.renderEmailForm() } />
+              <Tab id="facebook" title="Facebook" panel={ this.renderFacebookForm() } />
+            </Tabs>
+        )
+    }
+    else {
+      return this.renderEmailForm()
+    }
+  }
+  private renderFacebookForm() {
+    return (
+      <React.Fragment>
+        {this.state.errorMessage !== null && <Callout intent={Intent.DANGER}>
+          {this.state.errorMessage}
+        </Callout> }
+
+        <div className="login-dialog-buttons">
+          <Button onClick={ () => this.props.onDismiss() }>Cancel</Button>
+          { this.props.onLoginWithFacebook && <Button icon={IconNames.LOG_IN} intent={Intent.PRIMARY}
+            onClick={ () => this.loginWithFacebook() }>Log In with your Facebook account</Button> }
+        </div>
+      </React.Fragment>
+    )
+  }
+
+  private renderEmailForm() {
+    return (
+      <React.Fragment>
+        {this.state.errorMessage !== null && <Callout intent={Intent.DANGER}>
+          {this.state.errorMessage}
+        </Callout> }
+
+        <FormGroup
+            label="Email"
+        >
+          <InputGroup id="username-input" placeholder="youremail@sailing.dream" leftIcon={ IconNames.USER }
+            value={this.state.username} onChange={ (e:any) => this.setState({ username: e.target.value }) }/>
+        </FormGroup>
+        <FormGroup
+            label="Password"
+        >
+          <InputGroup id="password-input" type="password" leftIcon={ IconNames.LOCK }
+            value={this.state.password} onChange={ (e:any) => this.setState({ password: e.target.value }) }/>
+        </FormGroup>
+
+        <div className="login-dialog-buttons">
+          <Button onClick={ () => this.props.onDismiss() }>Cancel</Button>
+          <Button icon={IconNames.LOG_IN} onClick={ () => this.createAccountWithEmailAndPassword() }>Create account</Button>
+          <Button icon={IconNames.LOG_IN} intent={Intent.PRIMARY} onClick={ () => this.loginWithEmailAndPassword() }>Log in</Button>
+        </div>
+      </React.Fragment>
+    )
   }
 
   private loginWithEmailAndPassword() {
